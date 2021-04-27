3b8654e4e56dc57609325ae43b59c9fd7cb185a6
2known <61994491+nasdeen@users.noreply.github.com>
2021-04-27 23:55:31 +0000

    Update MovieReview on Tuesday
    I updated this file based on the modal display for each movie review. the modal was moved from the result.jsx page here.

--- src/Component/MovieReview.jsx
+++ src/Component/MovieReview.jsx
@@ -1,17 +1,28 @@
 import React, { useState } from "react";
+import { Modal, Button } from "react-bootstrap";
 
 const MovieReview = ({ item }) => {
-  // const [newItems, setNewItems] = useState([item.summary_short]);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div>
-      {item.map((eachItem, index) => {
-        return (
-          <div>
-            {eachItem.summary_short}
-            {index}
-          </div>
-        );
-      })}
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Title>Movie Review</Modal.Title>
+
+        <Modal.Body>{item.summary_short}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Button className="btn btn-primary" onClick={handleShow}>
+        Movie Review
+      </Button>
+
+     
     </div>
   );
 };
