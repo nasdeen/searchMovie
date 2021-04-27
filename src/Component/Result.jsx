5793ef9fd66771844472c3d920745f4cd8bfe653
2known <61994491+nasdeen@users.noreply.github.com>
2021-04-27 23:52:26 +0000

    Update Resulte on Tuesday
    I have to move the modal window from the Result.jsx to MovieReview with the help of the MovieReview component to make it possible to pass individual data for modal to display a particular Movie Review by a click.

--- src/Component/Result.jsx
+++ src/Component/Result.jsx
@@ -1,33 +1,11 @@
-import React,{useState} from "react";
-import { Button, Modal } from "react-bootstrap";
+import React from "react";
 import MovieReview from "./MovieReview";
 
 const Result = ({ result }) => {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
   return (
     <>
-    <Modal show={show} onHide={handleClose}>
-        <Modal.Title>Movie Review</Modal.Title>
-
-        <Modal.Body>
-          <MovieReview item={result} />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-      <div
-        style={{
-          backgroundColor: "#b3d7ff",
-        }}
-      >
-        {result.map((item) => {
+      <div style={{ backgroundColor: "#b3d7ff" }}>
+        {result.map((item, index) => {
           return (
             <div style={{ padding: 30 }}>
               <p style={{ textAlign: "left" }}>
@@ -35,7 +13,7 @@ const Result = ({ result }) => {
               </p>
               <h3>{item.headline}</h3>
               <h4>{item.display_title}</h4>
-              <Button className="btn btn-primary" onClick={handleShow}>Movie Review </Button>
+              <MovieReview item={item} key={index} />
             </div>
           );
         })}
